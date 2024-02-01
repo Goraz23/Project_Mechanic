a14e23c1a7da714ff7d1775c0e7a14a2e5de0fba
Geovani <geovani.guzman01@gmail.com>
2024-02-01 03:20:17 -0600

    Apis

--- BACK/server.js
+++ BACK/server.js
@@ -3,6 +3,7 @@ import mysql from 'mysql';
 import cors from 'cors';
 
 const app = express();
+app.use(express.json());
 
 app.use(cors());
 
@@ -21,6 +22,34 @@ connection.connect((error) => {
     }
 });
 
+app.post('/addMechanic',(req,res)=>{
+
+    const alias = req.body.alias
+    const surname = req.body.surname
+    const email = req.body.email
+    const pass = req.body.pass
+    const rol_id = req.body.rol_id
+
+    const AddMechanic = `INSERT INTO mechanic (alias, surname, email, pass, rol_id) VALUES(?, ?, ?, ?, ?)`
+
+    connection.query(AddMechanic,[alias, surname, email, pass, rol_id], (err, result)=>{
+
+        if(err) return res.json({error: 'Error al agregar mecanico', err})
+         return res.json({usuarios: result})
+
+    })
+})
+
+app.get('/viewMechanic',(req,res)=>{
+    const viewMechanic = `SELECT * FROM mechanic WHERE rol_id = 1`
+    connection.query(viewMechanic,(err,result)=>{
+        if(err)  return res.json({error: 'error al ver los mecanicos', err})
+        return res.json({mecanicos: result})
+    })
+})
+
 app.listen(8082, () => {
     console.log('ESCUCHANDO EN EL PUERTO 8082');
 });
+
+
