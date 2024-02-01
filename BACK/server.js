1e9bffbfb015e1126fabdd63f57af0bf02319351
Geovani <geovani.guzman01@gmail.com>
2024-02-01 10:03:35 -0600

    endpoint

--- BACK/server.js
+++ BACK/server.js
@@ -40,6 +40,16 @@ app.post('/addMechanic',(req,res)=>{
     })
 })
 
+app.get('/getUsers/id',(req,res)=>{
+
+    const id_mechanic = req.body.id_mechanic
+    const getUsers = `SELECT * FROM mechanic id_mechanic = ?`
+    connection.query(getUsers,[id_mechanic],(err,result)=>{
+        if(err)  return res.json({error: 'error al ver los mecanicos', err})
+        return res.json({usuarios: result})
+    })
+})
+
 app.get('/viewMechanic',(req,res)=>{
     const viewMechanic = `SELECT * FROM mechanic WHERE rol_id = 1`
     connection.query(viewMechanic,(err,result)=>{
@@ -47,6 +57,43 @@ app.get('/viewMechanic',(req,res)=>{
         return res.json({mecanicos: result})
     })
 })
+
+app.delete('/deleteMechanic/:id', (req, res) => {
+    const mechanicId = req.params.id; 
+
+    const deleteMechanic = `DELETE FROM mechanic WHERE id_mechanic = ?`; 
+    connection.query(deleteMechanic, [mechanicId], (err, result) => {
+        if (err) return res.json({ error: 'error al eliminar mecanicos', err });
+        return res.json({ mecanicos: result });
+    });
+});
+
+app.post('/addMaterial',(req,res)=>{
+
+    const material = req.body.material
+    const precio = req.body.precio
+    const cantidad = req.body.cantidad
+
+    const addMaterial = `INSERT INTO materials (material, precio, cantidad) VALUES(?, ?, ?)`
+
+    connection.query(addMaterial,[material, precio, cantidad],(err,result)=>{
+        if(err) return res.json({error: 'error al agregar material', err})
+        return res.json({material: result})
+    })
+
+})
+
+app.get('/viewMaterial',(req,res)=>{
+    const viewMaterial = `SELECT * FROM materials`
+    connection.query(viewMaterial,(err,result)=>{
+        if(err)  return res.json({error: 'error al ver los mecanicos', err})
+        return res.json({materiales: result})
+    })
+})
+
+
+
+
 
 app.listen(8082, () => {
     console.log('ESCUCHANDO EN EL PUERTO 8082');
