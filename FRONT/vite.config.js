6f4df1e7e06df7d9b1493fd0c23b7e6b6cf598f5
ricardojch.12@gmail.com <ricardojch.12@gmail.com>
2024-01-31 21:02:13 -0500

    Integracion de SSL

--- FRONT/vite.config.js
+++ FRONT/vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    https:{
+      key: './front-privateKey.key',
+      cert: './front.crt',
+    }
+  },
   plugins: [react()],
 })
