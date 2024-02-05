47cc8bd256e2b19f2a8452ee03f5df8313042def
ricardojch.12@gmail.com <ricardojch.12@gmail.com>
2024-02-05 15:32:04 -0500

    BACK Y FRONT  ADMIN_ VEHICULOS

--- FRONT/src/pages/admin/vehiculos_admin.jsx
+++ FRONT/src/pages/admin/vehiculos_admin.jsx
@@ -1,35 +1,138 @@
-import React from "react";
+// import axios from "axios";
+import React, { useEffect, useState } from "react";
 import Tupla from "../../components/tupla";
 import Boton_agregar from "../../components/boton_agregar";
 import Navbar_admin from "../../components/admin/navbar_admin";
-function vehiculos_admin() {
+
+function Vehiculos_admin() {
+  const [vehiculos, setVehiculos] = useState({
+    placa: "",
+    modelo: "",
+    descripcion: "",
+  });
+
+  const [viewVehiculo, setviewVehiculo] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8082/viewVehiculo", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((vehiculos) => {
+        setviewVehiculo(vehiculos.vehiculos);
+      })
+      .catch((error) => console.log("Error fetching data:", error));
+  }, [viewVehiculo]);
+
+  const addVehiculo = async (e) => {
+    try {
+      const response = await fetch("http://localhost:8082/addVehiculo", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(vehiculos),
+      });
+
+      const result = await response.json();
+      console.log(result);
+
+      setVehiculos({
+        placa: "",
+        modelo: "",
+        descripcion: "",
+      });
+      alert("Se ha agregado el vehículo correctamente");
+      if (result.success) {
+        setviewVehiculo([...viewVehiculo, vehiculos]);
+      } else {
+        alert("No se pudo agregar el vehículo");
+        console.error("Error al registrar vehiculo");
+      }
+    } catch (error) {
+      console.error("Error al registar", error);
+    }
+  };
+
+  const delateVehiculo = async (id_vehiculos) => {
+    try {
+      const response = await fetch(
+        `http://localhost:8082/delateVehiculo/${id_vehiculos}`,
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            id_vehiculos: id_vehiculos,
+          }),
+        }
+      );
+
+      const result = await response.json();
+      console.log(result);
+
+      // Después de eliminar, actualiza la lista de mecánicos
+      if (result.success) {
+        setviewVehiculo(
+          viewVehiculo.filter(
+            (vehiculo) => vehiculo.id_vehiculos !== id_vehiculos
+          )
+        );
+      } else {
+        alert("El vehículo no fue encontrado");
+        console.error("Error al eliminar vehiculo");
+      }
+    } catch (error) {
+      console.error("Error al eliminar ", error);
+    }
+  };
+
+  const valueChange = (e, values) => {
+    setVehiculos({
+      ...vehiculos,
+      [values]: e.target.value,
+    });
+  };
+
+  console.log(vehiculos);
+  console.log(viewVehiculo);
+
   return (
     <>
-    <Navbar_admin />
+      <Navbar_admin />
       <div className="mt-5 w-[%100] h-full mx-96 bg-[#FFF] items-center">
         <div className="items-center ">
           <Tupla
             tupla="Placa del auto: "
             descripcion="Ingresa la placa del auto"
-            
+            dato="text"
+            value={vehiculos.placa}
+            change={(e) => valueChange(e, "placa")}
           />
         </div>
         <div className="items-center place-items-center ">
-          <Tupla tupla="Modelo: " descripcion="Ingresar el modelo" />
+          <Tupla
+            tupla="Modelo: "
+            descripcion="Ingresar el modelo"
+            dato="text"
+            value={vehiculos.modelo}
+            change={(e) => valueChange(e, "modelo")}
+          />
         </div>
         <div className="items-center place-items-center ">
           <Tupla
             tupla="Descripción: "
             descripcion="Ingresar características del auto"
+            dato="text"
+            value={vehiculos.descripcion}
+            change={(e) => valueChange(e, "descripcion")}
           />
         </div>
-        <div className="items-center place-items-center ">
-          <Tupla
-            tupla="Trabajador: "
-            descripcion="Ingresar con que trabajador trabajará"
-          />
-        </div>
-        <Boton_agregar agregar="Agrega auto" />
+        <Boton_agregar subir={addVehiculo} agregar="Agrega Vehiculo" />
       </div>
       <div className="mt-5 mx-20 border-separate border border-slate-[#185866] bg-[#B2C9CE]  rounded-t-lg items-center">
         <table className="w-full table-auto bg-[#B2C9CE] rounded-t-lg">
@@ -39,27 +142,42 @@ function vehiculos_admin() {
               <th className="p-2">Placas del auto</th>
               <th className="p-2">Modelo</th>
               <th className="p-2">Descripción</th>
-              <th className="p-2">Trabajador</th>
               <th className="p-2">Acciones</th>
             </tr>
           </thead>
           <tbody className="text-center bg-white">
-            <tr>
-              <td>1</td>
-              <td>placa1</td>
-              <td>k1</td>
-              <td>Es azul con....</td>
-              <td>Mario</td>
-              <td>Editar/Eliminar</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>placa2</td>
-              <td>Lizan2</td>
-              <td>Blanco con ...</td>
-              <td>Panchito</td>
-              <td>Editar/Eliminar</td>
-            </tr>
+            {viewVehiculo.map((vehiculo) => (
+              <tr key={vehiculo.id_vehiculos}>
+                <td>{vehiculo.id_vehiculos}</td>
+                <td>{vehiculo.placa}</td>
+                <td>{vehiculo.modelo}</td>
+                <td>{vehiculo.descripcion}</td>
+                <td className="pt-2">
+                  <button
+                    type="button"
+                    onClick={() => delateVehiculo(vehiculo.id_vehiculos)}
+                    className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                  >
+                    <box-icon
+                      name="trash"
+                      type="solid"
+                      color="#ffffff"
+                      className="items-center"
+                    ></box-icon>
+                  </button>
+                  <button
+                    type="button"
+                    className="text-white bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:focus:ring-yellow-900"
+                  >
+                    <box-icon
+                      name="info-circle"
+                      type="solid"
+                      color="#ffffff"
+                    ></box-icon>
+                  </button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
@@ -67,4 +185,4 @@ function vehiculos_admin() {
   );
 }
 
-export default vehiculos_admin;
+export default Vehiculos_admin;
