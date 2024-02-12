13f25f72dcba7ff51c3dc2ec90ef16c110977678
ricardojch.12@gmail.com <ricardojch.12@gmail.com>
2024-02-12 18:02:05 -0500

    APIs Login

--- BACK/server.js
+++ BACK/server.js
@@ -1,12 +1,18 @@
+// ! Importaciones  generales
 import express from "express";
 import mysql from "mysql";
 import cors from "cors";
+import nodemailer from "nodemailer"
+
+// ! Configuracion de la variable global
+let codigo;
 
 const app = express();
 app.use(express.json());
-
 app.use(cors());
 
+
+// ! Conexion a BD
 const connection = mysql.createConnection({
   host: "mysql-geovani.alwaysdata.net",
   user: "geovani",
@@ -27,28 +33,141 @@ app.listen(8082, () => {
 });
 
 
+// ! ENVIAR CORREO Y CODIGO DE VERIFICACIÓN USANDO  NODEMAILER
+
+const enviarMail = async(destinatario, coding) => {
+
+  const config = {
+      host: 'smtp.gmail.com',
+      port: 587,
+      auth: {
+          user: 'mechanicproyecto@gmail.com',
+          pass: 'gnpb xjnt mtfg tsgj',
+      }
+  }
+  const mensaje = {
+      from: 'mechanicproyecto@gmail.com',
+      to: `${destinatario}`,
+      subject: 'CONFIRMACIÓN DE ACCESO - MECHANIC',
+      text: `!Hola! Detectamos que estas intentando iniciar sesion en Mechanic, para confirmar que eres tu, ingresa el siguiente codigo: 
+      | "${coding}" |`
+
+  }
+  const transport = nodemailer.createTransport(config);
+
+  const info =  await transport.sendMail(mensaje);
+  console.log(info);
+}
+
+// ! GENERACION DE CÓDIGO
+function generarCodigoAleatorio() {
+const caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+codigo = "";
+
+for (let i = 0; i < 6; i++) {
+  const caracterAleatorio = caracteres.charAt(
+    Math.floor(Math.random() * caracteres.length)
+  );
+  codigo += caracterAleatorio;
+}
+
+return codigo;
+}
+app.post('/login', (request, response) => {
+const email = request.body.email;
+const password = request.body.pass;
+
+connection.query(
+  `SELECT mechanic.id_mechanic, mechanic.rol_id, mechanic.pass FROM mechanic WHERE email =? AND pass=?`,
+  [email,password],
+  async (error, result) => {
+    if (result.length === 0) {
+      response.status(200).json({
+        respuesta: "No se encontró un usuario con esos datos",
+        status: false,
+      });
+    } else {
+      const userId = result[0].id_mechanic;
+      response.status(200).json({
+        respuesta: result[0],
+        status: true,
+      });
+
+      generarCodigoAleatorio();
+
+      connection.query(
+        "UPDATE mechanic SET codigo = ? WHERE id_mechanic = ?;",
+        [codigo, userId],
+        (errors, results) => {
+          if (errors) {
+            console.error(
+              "Error al insertar el código de verificación en la base de datos:",
+              errors
+            );
+            response
+              .status(500)
+              .json({ error: "Error interno del servidor con el código de validación" });
+          } else {
+            console.log(
+              "Código de validación insertado correctamente en la base de datos"
+            );
+
+            enviarMail(email, codigo);
+
+          }
+        }
+      );
+    }
+  }
+);
+}
+);
 
 
-// ! API PARA EL LOGIN 
-app.post('/login', (req, res) => {
-  const  email = req.body.email
-  const pass  = req.body.pass
+// ! CONFRIMACIÓN DEL LOGIN 
+app.post ( "/confirmation", (request, response) => {
+const { id, codigo } = request.body;
+console.log("----> id: " + id + " codigo: " + codigo)
+
+connection.query(
+  'SELECT * FROM  mechanic WHERE id_mechanic = ? AND codigo = ?',
+  [id, codigo],
+  (error, results) => {
+    if (error) {
+      console.error("Error al actualizar el acceso del usuario: ", error);
+      response.status(500).json({ error: "Error interno del servidor al confirmar el login", status: false });
+    } else {
+      if (results.length > 0) {
+        console.log('Código del usuario es correcto');
+        response.status(200).json({ status: true, acceso: true, success: true, alias:results[0].alias, surname: results[0].surname , role: results[0].rol_id, token:'isAuth' });
+      } else {
+        console.log('Código del usuario es incorrecto');
+        response.status(200).json({ status: false });
+      }
+    }
+  }
+);
+});
+// // ! API PARA EL LOGIN 
+// app.post('/login', (req, res) => {
+//   const  email = req.body.email
+//   const pass  = req.body.pass
   
 
-  const query = 'SELECT * FROM mechanic WHERE email = ? AND pass = ?';
-  connection.query(query, [email, pass], (error, results) => {
+//   const query = 'SELECT * FROM mechanic WHERE email = ? AND pass = ?';
+//   connection.query(query, [email, pass], (error, results) => {
 
-    if (error) throw error;
-    if (results.length > 0) {
+//     if (error) throw error;
+//     if (results.length > 0) {
       
-      const user = results[0];
-      console.log(user)
-      res.json({ success: true, alias:user.alias, surname: user.surname , role: user.rol_id, token:'isAuth' });
-    } else { 
-      res.json({ success: false, message: 'Credenciales incorrectas' });
-    }
-  });
-});
+//       const user = results[0];
+//       console.log(user)
+//       res.json({ success: true, alias:user.alias, surname: user.surname , role: user.rol_id, token:'isAuth' });
+//     } else { 
+//       res.json({ success: false, message: 'Credenciales incorrectas' });
+//     }
+//   });
+// });
 
 
 // ! APIS PARA MECANICOS 
