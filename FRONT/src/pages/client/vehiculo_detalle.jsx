46578b83e1b0f276319963a18cd5cd2573f5b9f3
Geovani <geovani.guzman01@gmail.com>
2024-02-14 14:11:15 -0600

    nueva tabla

--- FRONT/src/pages/client/vehiculo_detalle.jsx
+++ FRONT/src/pages/client/vehiculo_detalle.jsx
@@ -2,6 +2,7 @@ import Navbar_mecanico from "../../components/client/navbar_mecanico";
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Boton_agregar from "../../components/boton_agregar";
+import Tupla from "../../components/tupla";
 
 function vehiculo_detalle() {
   const [selectedReparacion, setSelectedReparacion] = useState(0);
@@ -10,6 +11,13 @@ function vehiculo_detalle() {
   const [refresh, setRefresh] = useState(true);
   const [iniciarBloqueado, setIniciarBloqueado] = useState({});
   const [terminarBloqueado, setTerminarBloqueado] = useState({});
+  const [viewVM, setViewVM] = useState([]);
+  const [viewMaterial, setViewMaterial] = useState([])
+  const [viewVehiculo, setviewVehiculo] = useState([])
+  const [selectedVehiculo, setSelectedVehiculo] = useState(0);
+  const [selectedMaterial, setSelectedMaterial] = useState(0);
+  const [costoMaterial, setCostoMaterial] = useState(0)
+
 
   useEffect(() => {
     fetch("http://localhost:8082/viewReparaciones", {
@@ -41,10 +49,54 @@ function vehiculo_detalle() {
       .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
+
+  useEffect(() => {
+    fetch("http://localhost:8082/viewVM", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((vm) => {
+        setViewVM(vm.vehiculo_material[0]);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  console.log('materiale vehiculo', viewVM)
   useEffect(() => {
     fetch("http://localhost:8082/viewReparacion")
       .then((response) => response.json())
       .then((data) => setViewTipoR(data.reparaciones))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  useEffect(() => {
+    fetch("http://localhost:8082/viewVehiculo", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((vehiculos) => {
+        setviewVehiculo(vehiculos.vehiculos);
+      })
+      .catch((error) => console.log("Error fetching data:", error));
+  }, [refresh]);
+
+  useEffect(() => {
+    fetch("http://localhost:8082/viewMaterial", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((materiales) => {
+        setViewMaterial(materiales.materiales);
+      })
       .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
@@ -75,6 +127,35 @@ function vehiculo_detalle() {
     }
   };
 
+  const addVM = async () => {
+    try {
+      const reparacion_material = {
+        costo_material: costoMaterial,
+        materials_id: selectedMaterial ,
+        vehiculos_id: selectedVehiculo
+      };
+
+      const response = await fetch("http://localhost:8082/addVM", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(reparacion_material),
+      });
+      const result = await response.json();
+
+      if (!response.ok) {
+        console.error("Error al registrar trabajo", result.error);
+      } else {
+        setRefresh(!refresh);
+        setViewTipoR([...viewTipoR, result]);
+        setSelectedVehiculo("");
+        setSelectedMaterial("")
+      }
+    } catch (error) {
+      console.error("Error al registrar", error);
+    }
+  };
   const updateHoraFinal = async (id_reparacion_vehiculo) => {
     try {
       setTerminarBloqueado((prevState) => ({
@@ -138,25 +219,42 @@ function vehiculo_detalle() {
       const response = await fetch(
         `http://localhost:8082/deleteRV/${id_reparacion_vehiculo}`,
         {
-          method: "DELETE",
+          method: 'DELETE',
           headers: {
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json',
           },
-        }
-      );
+        });
       const result = await response.json();
 
       if (!response.ok) {
-        console.error("Error al eliminar trabajo", result.error);
+        console.error('Error al eliminar trabajo', result.error);
       } else {
-        setViewReparaciones(
-          viewReparaciones.filter(
-            (item) => item.id_reparacion_vehiculo !== id_reparacion_vehiculo
-          )
-        );
+        setViewReparaciones(viewReparaciones.filter((item) => item.id_reparacion_vehiculo !== id_reparacion_vehiculo));
       }
     } catch (error) {
-      console.error("Error al eliminar trabajo", error);
+      console.error('Error al eliminar trabajo', error);
+    }
+  };
+
+  const deleteVM = async (id_vehiculo_material) => {
+    try {
+      const response = await fetch(
+        `http://localhost:8082/deleteVM/${id_vehiculo_material}`,
+        {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+      const result = await response.json();
+
+      if (!response.ok) {
+        console.error('Error al eliminar trabajo', result.error);
+      } else {
+        setViewTipoR(viewTipoR.filter((item) => item.id_vehiculo_material !== id_vehiculo_material));
+      }
+    } catch (error) {
+      console.error('Error al eliminar trabajo', error);
     }
   };
 
@@ -258,7 +356,7 @@ function vehiculo_detalle() {
                         }
                         className="text-white bg-red-700 hover:bg-red-700 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
                         disabled={
-                          terminarBloqueado[item.id_reparacion_vehiculo]
+                          terminarBloqueado[item.id]
                         }
                       >
                         <box-icon
@@ -275,6 +373,7 @@ function vehiculo_detalle() {
             </table>
           </div>
         </div>
+        {/* --------------------------------------------------------------- */}
         <div className="w-[50%] m-5 border-2 border-cyan-900 border-opacity-5  rounded-xl h-full">
           <h1 className="text-center text-pretty text-2xl font-bold m-auto my-3 text-[#2a6673]">
             Materiales
@@ -282,22 +381,87 @@ function vehiculo_detalle() {
           <div className="text-right  ltr rounded-2xl h-10 w-[130px]  pr-3 pt-2 pb-8 right-0 ml-5 items-baseline pl-0  border">
             COSTO TOTAL
           </div>
+          <div className="pb-5">
+                <div className="items-center place-items-center ">
+                <Tupla
+                  tupla="Costo "
+                  descripcion="Ingresar costo..."
+                  dato="number"
+                  value={costoMaterial}
+                  change={(e) => setCostoMaterial(e.target.value)}
+                />
+              </div>
+              <div className="items-center p-3">
+              <select
+                  value={selectedVehiculo}
+                  onChange={(e) => setSelectedVehiculo(e.target.value)}
+                  className="bg-gray-50 border-[2px] text-gray-900 text-sm rounded-3xl focus:ring-blue-500 border-[#185866] focus:border-blue-500 block w-full p-2 mt-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                >
+                  {viewVehiculo.map((vehiculo) => (
+                    <option
+                      key={vehiculo.id_vehiculos}
+                      value={vehiculo.id_vehiculos}
+                    >
+                      {vehiculo.placa}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="items-center p-3">
+              <select
+                  value={selectedMaterial}
+                  onChange={(e) => setSelectedMaterial(e.target.value)}
+                  className="bg-gray-50 border-[2px] text-gray-900 text-sm rounded-3xl focus:ring-blue-500 border-[#185866] focus:border-blue-500 block w-full p-2 mt-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                >
+                  {viewMaterial.map((material) => (
+                    <option
+                      key={material.id_materials}
+                      value={material.id_materials}
+                    >
+                      {material.material}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <Boton_agregar subir={addVM} agregar="Agregar" />
+            </div>
           <div className="m-5">
             <table className="w-full table-auto bg-[#B2C9CE] m-auto  rounded-t-lg">
               <thead className="text-center text-white ">
-                <tr className="">Id</tr>
-                <tr className="">Tipo de reparación</tr>
-                <tr className="">Hrs</tr>
-                <tr className="">Costo por hora</tr>
+                <th className="">#</th>
+                <th className="">Costo por hora</th>
+                <th className="">Materiales</th>
+                <th className="">Vehiculo</th>
               </thead>
 
               <tbody className="text-center bg-white">
-                <tr>
-                  <td>1</td>
-                  <td> REVISION</td>
-                  <td>13 HORAS</td>
-                  <td>450 PESOS</td>
-                </tr>
+              {viewVM.map((item, index) => (
+                  <tr key={index}>
+                    <td>{index + 1}</td>
+                    <td>{item.costo_material}</td>
+                    <td>{item.material}</td>
+                    <td>{item.placa}</td>
+                    <td className="pt-2">
+                      <button
+                        onClick={() =>
+                          deleteVM(item.id_vehiculo_material)
+                        }
+                        className="text-white bg-red-700 hover:bg-red-700 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                        disabled={
+                          terminarBloqueado[item.id]
+                        }
+                      >
+                        <box-icon
+                          name="trash"
+                          type="solid"
+                          color="#ffffff"
+                          className="items-center"
+                        ></box-icon>
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+                
               </tbody>
             </table>
           </div>
